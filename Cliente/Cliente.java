d1d56b828f1b5f7013dfca1d9066476927605cf2
SeijiNoda <49851925+SeijiNoda@users.noreply.github.com>
2019-11-05 08:42:07 -0300

    Update Cliente.java

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -8,101 +8,103 @@ public class Cliente
 
 	public static void main (String[] args)
 	{
-        if (args.length>2)
-        {
-            System.err.println ("Uso esperado: java Cliente [HOST [PORTA]]\n");
-            return;
-        }
+		if (args.length>2)
+		{
+		    System.err.println ("Uso esperado: java Cliente [HOST [PORTA]]\n");
+		    return;
+        	}
 
-        Socket conexao=null;
-        try
-        {
-            String host = Cliente.HOST_PADRAO;
-            int    porta= Cliente.PORTA_PADRAO;
+		Socket conexao=null;
+		try
+		{
+		    String host = Cliente.HOST_PADRAO;
+		    int    porta= Cliente.PORTA_PADRAO;
 
-            if (args.length>0)
-                host = args[0];
+		    if (args.length>0)
+			host = args[0];
 
-            if (args.length==2)
-                porta = Integer.parseInt(args[1]);
-System.out.println (host+" "+porta);
-            conexao = new Socket (host, porta);
-        }
-        catch (Exception erro)
-        {			erro.printStackTrace();
+		    if (args.length==2)
+			porta = Integer.parseInt(args[1]);
+		    System.out.println (host+" "+porta);
+		    conexao = new Socket (host, porta);
+		}
+		catch (Exception erro)
+		{			
+		    erro.printStackTrace();
 
-            System.err.println ("Indique o servidor e a porta corretos!\n");
-            return;
-        }
+		    System.err.println ("Indique o servidor e a porta corretos!\n");
+		    return;
+		}
 
-        ObjectOutputStream transmissor=null;
-        try
-        {
-            transmissor =
-            new ObjectOutputStream(
-            conexao.getOutputStream());
-        }
-        catch (Exception erro)
-        {			erro.printStackTrace();
+		ObjectOutputStream transmissor=null;
+		try
+		{
+		    transmissor =
+		    new ObjectOutputStream(
+		    conexao.getOutputStream());
+		}
+		catch (Exception erro)
+		{			
+		    erro.printStackTrace();
 
-            System.err.println ("Indique o servidor e a porta corretos!\n");
-            return;
-        }
+		    System.err.println ("Indique o servidor e a porta corretos!\n");
+		    return;
+		}
 
-        ObjectInputStream receptor=null;
-        try
-        {
-            receptor =
-            new ObjectInputStream(
-            conexao.getInputStream());
-        }
-        catch (Exception erro)
-        {			erro.printStackTrace();
+		ObjectInputStream receptor=null;
+		try
+		{
+		    receptor =
+		    new ObjectInputStream(
+		    conexao.getInputStream());
+		}
+		catch (Exception erro)
+		{			
+		    erro.printStackTrace();
 
-            System.err.println ("Indique o servidor e a porta corretos!\n");
-            return;
-        }
+		    System.err.println ("Indique o servidor e a porta corretos!\n");
+		    return;
+		}
 
-        Parceiro servidor=null;
-        try
-        {
-            servidor =
-            new Parceiro (conexao, receptor, transmissor);
-        }
-        catch (Exception erro)
-        {			erro.printStackTrace();
+		Parceiro servidor=null;
+		try
+		{
+		    servidor = new Parceiro (conexao, receptor, transmissor);
+		}
+		catch (Exception erro)
+		{			
+		    erro.printStackTrace();
 
-            System.err.println ("Indique o servidor e a porta corretos!\n");
-            return;
-        }
+		    System.err.println ("Indique o servidor e a porta corretos!\n");
+		    return;
+		}
 
-        char opcao=' ';
-        do
-        {
-            System.out.print ("Sua opcao (" +
-                              "A=Adicao/" +
-                              "S=Subtracao/" +
-                              "M=Multiplicacao/" +
-                              "D=Divisao/" +
-                              "V=Ver valor/" +
-                              "T=Terminar)" +
-                              "? ");
+		char opcao=' ';
+		do
+		{
+		    System.out.print ("Sua opcao (" +
+				      "A=Adicao/" +
+				      "S=Subtracao/" +
+				      "M=Multiplicacao/" +
+				      "D=Divisao/" +
+				      "V=Ver valor/" +
+				      "T=Terminar)" +
+				      "? ");
 
-            try
-            {
-				opcao = Character.toUpperCase(Teclado.getUmChar());
+		    try
+		    {
+			opcao = Character.toUpperCase(Teclado.getUmChar());
 		    }
 		    catch (Exception erro)
 		    {
-				System.err.println ("Opcao invalida!\n");
-				continue;
-			}
-
-			if ("ASMDVT".indexOf(opcao)==-1)
-		    {
-				System.err.println ("Opcao invalida!\n");
-				continue;
-			}
+			System.err.println ("Opcao invalida!\n");
+			continue;
+		    }
+		   if ("ASMDVT".indexOf(opcao)==-1)
+		   {
+			System.err.println ("Opcao invalida!\n");
+			continue;
+		   }
 
 			try
 			{
@@ -152,8 +154,7 @@ System.out.println (host+" "+porta);
 				System.err.println ("Caso o erro persista, termine o programa");
 				System.err.println ("e volte a tentar mais tarde!");
 			}
-        }
-        while (opcao != 'T');
-
-    }
+		}
+		while (opcao != 'T');
+    	}
 }
