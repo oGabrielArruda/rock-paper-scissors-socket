5dde61ac510af6e60aac108e1efbf37c1dee24fe
antoniokot <55291669+antoniokot@users.noreply.github.com>
2019-11-18 13:41:25 -0300

    JAVADOCS - ComunicadoDesligamento

--- Servidor/ComunicadoDeDesligamento.java
+++ Servidor/ComunicadoDeDesligamento.java
@@ -1,2 +1,8 @@
+/**
+A classe ComunicadoDesligamento é responsável por desligar a aplicação.
+@author Antônio Hideto Borges Kotsubo & Gabriel Alves Arruda & Matheus Seiji Luna Noda & Nouani Gabriel Sanches.
+@since 2019.
+*/
+
 public class ComunicadoDeDesligamento extends Comunicado
 {}
