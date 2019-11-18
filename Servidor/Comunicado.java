70909fa49589383569e4cb2a86e4e95c2223c06f
antoniokot <55291669+antoniokot@users.noreply.github.com>
2019-11-18 13:40:06 -0300

    JAVADOCS - Comunicado

--- Servidor/Comunicado.java
+++ Servidor/Comunicado.java
@@ -4,6 +4,7 @@ import java.io.*;
 A classe Comunicado é responsável por originar os diferentes comunicados.
 @author Antônio Hideto Borges Kotsubo & Gabriel Alves Arruda & Matheus Seiji Luna Noda & Nouani Gabriel Sanches.
 @see java.io.*.
+@since 2019.
 */
 
 public class Comunicado implements Serializable, Cloneable
