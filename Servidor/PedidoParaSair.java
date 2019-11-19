3d9127b57196142fd04b56a2c53fdf2d8e726fae
antoniokot <55291669+antoniokot@users.noreply.github.com>
2019-11-19 11:44:20 -0300

    JAVADOCS - PedidoParaSair

--- Servidor/PedidoParaSair.java
+++ Servidor/PedidoParaSair.java
@@ -1,5 +1,5 @@
 /**
-A classe PedidoParaSair representa o desejo do usuário de sair da aplicação
+A classe PedidoParaSair representa o desejo do usuário de sair da aplicação.
 @author Antônio Hideto Borges Kotsubo & Gabriel Alves Arruda & Matheus Seiji Luna Noda & Nouani Gabriel Sanches.
 @since 2019.
 */
