d0e2926e3a05a8da5cba48b4c2c29d6e9032a740
antoniokot <55291669+antoniokot@users.noreply.github.com>
2019-11-19 10:49:20 -0300

    JAVADOCS - PedidoDeNome

--- Servidor/PedidoDeNome.java
+++ Servidor/PedidoDeNome.java
@@ -1,6 +1,19 @@
+/**
+A classe PedidoDeNome representa uma escolha de nome feita pelo usuário.
+@author Antônio Hideto Borges Kotsubo & Gabriel Alves Arruda & Matheus Seiji Luna Noda & Nouani Gabriel Sanches.
+@since 2019
+*/
+
 public class PedidoDeNome extends Comunicado
 {
 	private String nome;
+
+	/**
+	Constroi uma nova instância da classe PedidoDeNome.
+	Para tal, é necessário um parâmetro da classe String 
+	que contenha o nome desejado pelo usuário.
+	@param nome	Uma String com o nome desejado pelo usuário.
+	*/
 
 	public PedidoDeNome(String nome)
 	{
